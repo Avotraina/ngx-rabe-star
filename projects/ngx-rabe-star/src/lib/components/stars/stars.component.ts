c1b365aa840d021fc34f4f6b5bd590ced5f9e6e7
Avotraina <avotrainarabe@gmail.com>
2024-10-10 13:53:41 +0300

    Adding half star

--- projects/ngx-rabe-star/src/lib/components/stars/stars.component.ts
+++ projects/ngx-rabe-star/src/lib/components/stars/stars.component.ts
@@ -1,45 +1,91 @@
-import { Component, Input, input, output, signal } from '@angular/core';
-import { I_StarInterface } from '../../types/star.interface';
+import { Component, forwardRef, Input, input, output, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'ngx-rabe-star',
   standalone: true,
   imports: [CommonModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => RabeStars),
+      multi: true,
+    },
+  ],
   templateUrl: './stars.component.html',
   styleUrl: './stars.component.css'
 })
-export class RabeStars {
+export class RabeStars implements ControlValueAccessor {
 
   title = 'rabe-star';
 
   rating = signal<number>(0);
   hoverRating: number | null = null;
+  stars: number[] = [1, 2, 3, 4, 5];
 
   private disableStars = false;
+  protected isDisabled: boolean = false;
   @Input() disabled: boolean = false;
   @Input() customClass: string = '';
+  @Input() readOnly: boolean = false;
 
   ratingChange = output<number>();
 
   private onTouched?() { }
   public onChange(star: number) { }
 
-  protected rate(star: number): void {
-    if (this.rating() == star) {
-      this.rating.set(0);
-      this.onChange(0);
-      this.ratingChange.emit(0);
+  protected rate(star: number, event: MouseEvent): void {
+
+    const rect = (event.target as HTMLElement).getBoundingClientRect();
+    const halfWidth = rect.width / 2;
+    const mouseX = event.clientX - rect.left;
+
+    if (mouseX < halfWidth) {
+
+      // cancel rating when mouse click twice on same half star
+      if (this.rating() == star - 0.5) {
+        this.rating.set(0);
+        this.onChange(0);
+        this.ratingChange.emit(0);
+
+        return;
+      }
+
+      this.rating.set(star - 0.5);
+      this.onChange(star - 0.5);
+      this.ratingChange.emit(star - 0.5);
+
     } else {
+
+      // cancel rating when mouse click twice on same star
+      if (this.rating() == star) {
+        this.rating.set(0);
+        this.onChange(0);
+        this.ratingChange.emit(0);
+
+        return;
+      }
+
       this.rating.set(star);
       this.onChange(star);
       this.ratingChange.emit(star);
+
     }
 
   }
 
-  setHoverRating(star: number): void {
-    this.hoverRating = star;
+  setHoverRating(star: number, event: MouseEvent): void {
+    const rect = (event.target as HTMLElement).getBoundingClientRect();
+    const halfWidth = rect.width / 2;
+    const mouseX = event.clientX - rect.left;
+
+    if (mouseX < halfWidth) {
+      this.hoverRating = star - 0.5;
+    } else {
+      this.hoverRating = star;
+    }
+
   }
 
   clearHoverRating(): void {
@@ -73,7 +119,6 @@ export class RabeStars {
 
   writeValue(obj: number): void {
     this.rating.set(obj);
-    // throw new Error('Method not implemented.');
   }
   registerOnChange(fn: any): void {
     this.onChange = fn;
@@ -81,12 +126,10 @@ export class RabeStars {
 
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
-    // throw new Error('Method not implemented.');
   }
 
   setDisabledState?(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-    // throw new Error('Method not implemented.');
+    this.isDisabled = isDisabled;
   }
 
 }
